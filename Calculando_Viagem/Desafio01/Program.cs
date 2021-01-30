6d9b0f9a722c3e9b3d9fee338026d4fb99120518
Jose Wesley <jwes.ofc1994@gmail.com>
2021-01-30 00:16:21 -0300

    primeiroDesafio

---
+++ Calculando_Viagem/Desafio01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Desafio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] viagem = Console.ReadLine().Split(' ');
+            int hour = Int32.Parse(viagem[0]);
+            int velocidade = Int32.Parse(viagem[1]);
+
+            double kmRodado = hour * velocidade;
+            double litrosCombustivel = kmRodado/12;
+
+            Console.WriteLine(litrosCombustivel.ToString("f3"));
+  
+        }
+    }
+}
